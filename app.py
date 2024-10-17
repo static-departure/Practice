1f191de21fa798eb9b6c90e4b3c367887f7de6af
static-departure <justinshava@gmail.com>
2024-10-17 16:11:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def payment_failure():
     return render_template('failure.html', message="Payment Failed. Please try again or contact support.")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
